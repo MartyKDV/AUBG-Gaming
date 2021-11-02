5e635053fb16f9f6bd7de3fa1890d9f9e6abbbd7
MartyKDV <martokadrev@gmail.com>
2021-11-02 17:45:30 +0200

    Search Implementing Costs

--- src/controllers/routes.go
+++ src/controllers/routes.go
@@ -11,6 +11,7 @@ func (server *Server) initialiseRoutes() {
 	server.Router.Handle("/product/{id}", server.isLogged(server.handleProductID)).Methods("GET")
 	server.Router.HandleFunc("/register", server.handleRegister)
 	server.Router.HandleFunc("/login", server.handleLogin)
+	server.Router.HandleFunc("/search", server.handleSearch)
 	server.Router.Handle("/cart", server.isLogged(server.handleCart)).Methods("GET")
 	server.Router.Handle("/cart/{id}", server.isLogged(server.handleCart)).Methods("POST")
 	server.Router.Handle("/cart/quantity/{id}", server.isLogged(server.handleCartUpdate)).Methods("POST")
